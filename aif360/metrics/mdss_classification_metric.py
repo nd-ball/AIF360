ea001d9ab8419801855a03177b2c80a12d397849
Samuel Hoffman <hoffman.sc@gmail.com>
2022-02-01 10:52:48 -0500

    Clean up bias scan
    * Remove dependency on casting floats to bools
    * Improve args in sklearn api
    * Fix docstrings
    * Clarify notebooks
    * Add new notebook reproducing paper analysis
    
    Signed-off-by: Samuel Hoffman <hoffman.sc@gmail.com>

--- aif360/metrics/mdss_classification_metric.py
+++ aif360/metrics/mdss_classification_metric.py
@@ -8,38 +8,63 @@ from aif360.metrics.mdss.MDSS import MDSS
 import pandas as pd
 
 class MDSSClassificationMetric(ClassificationMetric):
-    """
-        Bias subset scanning is proposed as a technique to identify bias in predictive models using subset scanning [1].
-        This class is a wrapper for the bias scan scoring and scanning methods that uses the ClassificationMetric abstraction.
+    """Bias subset scanning is proposed as a technique to identify bias in
+    predictive models using subset scanning [#zhang16]_.
+
+    This class is a wrapper for the bias scan scoring and scanning methods that
+    uses the ClassificationMetric abstraction.
+
     References:
-        .. [1] Zhang, Z., & Neill, D. B. (2016). Identifying significant predictive bias in classifiers. arXiv preprint arXiv:1611.08292.
+        .. [#zhang16] `Zhang, Z. and Neill, D. B., "Identifying significant
+           predictive bias in classifiers," arXiv preprint, 2016.
+           <https://arxiv.org/abs/1611.08292>`_
     """
-    def __init__(self, dataset: BinaryLabelDataset, classified_dataset: BinaryLabelDataset, 
-                scoring_function: ScoringFunction = Bernoulli(direction='positive'), unprivileged_groups: dict = None, privileged_groups:dict = None):
-    
-        super(MDSSClassificationMetric, self).__init__(dataset, classified_dataset,
-                                                       unprivileged_groups=unprivileged_groups,
-                                                       privileged_groups=privileged_groups)
-        
-        self.scanner = MDSS(scoring_function)
-    
-    def score_groups(self, privileged=True, penalty = 1e-17):
+    def __init__(self,
+                 dataset: BinaryLabelDataset,
+                 classified_dataset: BinaryLabelDataset,
+                 scoring_function: ScoringFunction = Bernoulli(direction=None),
+                 unprivileged_groups: dict = None,
+                 privileged_groups: dict = None):
         """
-        compute the bias score for a prespecified group of records.
-        
-        :param privileged: flag for group to score - privileged group (True) or unprivileged group (False).
-        This abstract the need to explicitly specify the direction of bias to scan for which depends on what the favourable label is.
-        :param penalty: penalty term. Should be positive. The penalty term as with any regularization parameter may need to be 
-        tuned for ones use case. The higher the penalty, the less complex (number of features and feature values) the highest scoring
-        subset that gets returned is.
-        
-        :returns: the score for the group
+        Args:
+            dataset (BinaryLabelDataset): Dataset containing ground-truth
+                labels.
+            classified_dataset (BinaryLabelDataset): Dataset containing
+                predictions.
+            scoring_function (ScoringFunction): Scoring function for MDSS. Note:
+                `direction` will be overridden.
+            privileged_groups (list(dict)): Privileged groups. Format is a list
+                of `dicts` where the keys are `protected_attribute_names` and
+                the values are values in `protected_attributes`. Each `dict`
+                element describes a single group. See examples for more details.
+            unprivileged_groups (list(dict)): Unprivileged groups in the same
+                format as `privileged_groups`.
+        """
+
+        super(MDSSClassificationMetric, self).__init__(
+            dataset, classified_dataset,
+            unprivileged_groups=unprivileged_groups,
+            privileged_groups=privileged_groups)
+
+        self.scanner = MDSS(scoring_function)
+
+    def score_groups(self, privileged=True, penalty=1e-17):
+        """Compute the bias score for a prespecified group of records.
+
+        Args:
+            privileged (bool): Flag for which direction to scan: privileged
+                (``True``) implies negative (observed worse than predicted
+                outcomes) while unprivileged (``False``) implies positive
+                (observed better than predicted outcomes).
+            penalty (float): Penalty term. Should be positive. The higher the
+                penalty, the less complex (number of features and feature
+                values) the highest scoring subset that gets returned is.
+
+        Returns:
+            float: Bias score for the given group.
         """
         groups = self.privileged_groups if privileged else self.unprivileged_groups
         subset = dict()
-        
-        xor_op = privileged ^ bool(self.classified_dataset.favorable_label)
-        direction = 'positive' if xor_op else 'negative'
 
         for g in groups:
             for k, v in g.items():
@@ -47,35 +72,40 @@ class MDSSClassificationMetric(ClassificationMetric):
                     subset[k].append(v)
                 else:
                     subset[k] = [v]
-        
+
         coordinates = pd.DataFrame(self.dataset.features, columns=self.dataset.feature_names)
         expected = pd.Series(self.classified_dataset.scores.flatten())
-        outcomes = pd.Series(self.dataset.labels.flatten())
-        
+        outcomes = pd.Series(self.dataset.labels.flatten() == self.dataset.favorable_label, dtype=int)
+
+        direction = 'negative' if privileged else 'positive'
         self.scanner.scoring_function.kwargs['direction'] = direction
         return self.scanner.score_current_subset(coordinates, expected, outcomes, dict(subset), penalty)
-    
-    def bias_scan(self, privileged=True, num_iters = 10, penalty = 1e-17):
-        """
-        scan to find the highest scoring subset of records
-        
-        :param privileged: flag for group to scan for - privileged group (True) or unprivileged group (False). 
-        This abstract the need to explicitly specify the direction of bias to scan for which depends on what the favourable label is.
-        :param num_iters: number of iterations (random restarts)
-        :param penalty: penalty term. Should be positive. The penalty term as with any regularization parameter may need to be 
-        tuned for ones use case. The higher the penalty, the less complex (number of features and feature values) the highest scoring
-        subset that gets returned is.
-        
-        :returns: the highest scoring subset and the score
-        """
 
-        xor_op = privileged ^ bool(self.classified_dataset.favorable_label)
-        direction = 'positive' if xor_op else 'negative'
-        self.scanner.scoring_function.kwargs['direction'] = direction
+    def bias_scan(self, privileged=True, num_iters=10, penalty=1e-17):
+        """Scan to find the highest scoring subset of records.
 
+        Args:
+            privileged (bool): Flag for which direction to scan: privileged
+                (``True``) implies negative (observed worse than predicted
+                outcomes) while unprivileged (``False``) implies positive
+                (observed better than predicted outcomes).
+            num_iters (int): Number of iterations (random restarts).
+            penalty (float): Penalty coefficient. Should be positive. The higher
+                the penalty, the less complex (number of features and feature
+                values) the highest scoring subset that gets returned is.
+
+        Returns:
+            tuple:
+                Highest scoring subset and its bias score
+
+                * **subset** (dict) -- Mapping of feature to value defining the
+                highest scoring subset.
+                * **score** (float) -- Bias score for that group.
+        """
         coordinates = pd.DataFrame(self.classified_dataset.features, columns=self.classified_dataset.feature_names)
-        
         expected = pd.Series(self.classified_dataset.scores.flatten())
-        outcomes = pd.Series(self.dataset.labels.flatten())
-        
+        outcomes = pd.Series(self.dataset.labels.flatten() == self.dataset.favorable_label, dtype=int)
+
+        direction = 'negative' if privileged else 'positive'
+        self.scanner.scoring_function.kwargs['direction'] = direction
         return self.scanner.scan(coordinates, expected, outcomes, penalty, num_iters)
